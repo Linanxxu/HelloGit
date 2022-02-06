b08095616b531829a4801cebca4b4ee240a4fea4
Linanxxu <458088496@qq.com>
2022-02-06 14:57:31 +0800

    第二次提交之前的更新

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -3,8 +3,10 @@ package com.atguigu.git;
 public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("hello git");
+		System.out.println("更新1");
 		System.out.println("masterLinan");
 		System.out.println("linan change code");
+		System.out.println("第二次提交之前更新");
 	}
 
 }
